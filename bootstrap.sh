d9771479fcaf78f94cc04c6152eb48e5e5973446
Le Kien Truc <truc.le@trustcircleglobal.com>
2016-07-30 09:23:52 +0700

    Add init project

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+VIRTUALENV_FOLDER='venv'
+virtualenv $VIRTUALENV_FOLDER
+if [ ! -d $VIRTUALENV_FOLDER ];
+then
+  virtualenv $VIRTUALENV_FOLDER
+fi
+. $VIRTUALENV_FOLDER/bin/activate
+pip install -r requirements.dev.txt
+pip install -r requirements.gae.txt
+pip install -r requirements.txt
+
